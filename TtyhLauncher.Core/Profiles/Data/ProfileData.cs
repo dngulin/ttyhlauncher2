e2630d851f65db607ff1c61394af9cf32c0a7ea6
Danil Gulin <dngulin@yandex.ru>
2019-07-18 23:43:48 +0300

    Add java settings into the profile data

--- TtyhLauncher.Core/Profiles/Data/ProfileData.cs
+++ TtyhLauncher.Core/Profiles/Data/ProfileData.cs
@@ -5,11 +5,21 @@ namespace TtyhLauncher.Profiles.Data {
     public class ProfileData {
         [JsonProperty("version")] public FullVersionId FullVersion = new FullVersionId("default", "");
         [JsonProperty("check_version")] public bool CheckVersionFiles = true;
+        
+        [JsonProperty("use_custom_java_path")] public bool UseCustomJavaPath;
+        [JsonProperty("custom_java_path")] public string CustomJavaPath = "";
+        
+        [JsonProperty("use_custom_java_args")] public bool UseCustomJavaArgs;
+        [JsonProperty("custom_java_args")] public string CustomJavaArgs = "";
 
         public ProfileData Clone() {
             return new ProfileData {
                 FullVersion = new FullVersionId(FullVersion.Prefix, FullVersion.Version),
-                CheckVersionFiles = CheckVersionFiles
+                CheckVersionFiles = CheckVersionFiles,
+                UseCustomJavaPath = UseCustomJavaPath,
+                CustomJavaPath = CustomJavaPath,
+                UseCustomJavaArgs = UseCustomJavaArgs,
+                CustomJavaArgs = CustomJavaArgs
             };
         }
     }
